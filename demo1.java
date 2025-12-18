44fb6980c69489462051b8378ddc28f4562eaccc
studymaterial9545-afk <studymaterial9545@gmail.com>
2025-12-19 02:06:13 +0530

    Update output message in demo1.java

--- demo1.java
+++ demo1.java
@@ -1,5 +1,5 @@
 public class demo1 {
   public static void main(String[] args) {
-    System.out.println("This is my 1st jenkins CI day 2 exited-added to pipeline");
+    System.out.println("This is my 1st jenkins CI day 2 exited-added to pipeline1");
   }
 }
